458172bf5629bfb2eae344cf2c50de41f74f3e55
IvanJackJack <ivancimino@hotmail.it>
2022-06-11 23:04:27 +0200

    reset files for git ignore

--- Playground/Intermediate/Build/Win64/PlaygroundEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.30.30711
-#include "C:/Users/ivanc/Desktop/Git repos/Unreal Repos/UnrealEngine5-Playground/Playground/Intermediate/Build/Win64/PlaygroundEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
